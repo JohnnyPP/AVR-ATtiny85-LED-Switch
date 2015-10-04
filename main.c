193b7d45614f00ea905b8d756277ce846090b024
JohnnyPP <johnnnyppp@gmail.com>
2015-10-04 20:35:43 +0200

    Resolved DDRB and PORTB symbols
    http://www.nongnu.org/avr-libc/user-manual/group__avr__sfr__notes.html
    http://stackoverflow.com/questions/13704076/passing-a-port-as-a-variable-avr

--- main.c
+++ main.c
@@ -12,23 +12,26 @@
 
 int main(void)
 {
-	DDRB |= 1 << PINB3;					// Set pin 3 to output
-	DDRB &= ~(1 << PINB4);				// Set pin 4 to input
-	PORTB |= 1 << PINB4;				// Set pin 4 to high
+	_SFR_IO8(0x17) |= 1 << PINB3;										// DDRB Set pin 3 to output
+	(*(volatile uint8_t *)((0x17) + 0x20)) &= ~(1 << PINB4);			// DDRB Set pin 4 to input
+	(*(volatile uint8_t *)((0x18) + 0x20)) |= 1 << PINB4;				// PORTB Set pin 4 to high
 
 	while(1)
 	{
-		PORTB ^= 1 << PINB3;			// Toggle pin 3 (LED)
+		// _SFR_IO8(0x18) and
+		// (*(volatile uint8_t *)((0x18) + 0x20)) and
+		// (*(volatile unsigned char *)((0x18) + 0x20)) are equivalent
+		(*(volatile unsigned char *)((0x18) + 0x20)) ^= 1 << PINB3;			// Toggle pin 3 (LED)
 
 		// Test whether bit PINB4 in IO register PINB is clear (Low).
 		// This will return TRUE if the bit is clear, and a FALSE if the bit is set (High).
 		if(bit_is_clear(PINB, PINB4))
 		{
-			_delay_ms(100);				// While the button is pressed (Button short-circuits PINB4 to the ground)
+			_delay_ms(100);		// While the button is pressed (Button short-circuits PINB4 to the ground)
 		}
 		else
 		{
-			_delay_ms(500);				// While the button is not pressed (PINB4 is set high)
+			_delay_ms(500);		// While the button is not pressed (PINB4 is set high)
 		}
 	}
 }
